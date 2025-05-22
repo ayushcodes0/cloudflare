80dd42aa1150c577ca8fa5a9603a51084180bc4c
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-05-22 22:40:53 +0530

    running the server

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,8 +2,16 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/', async(c) => {
+  // body, headers, query parameters, middlewares, connecting to a database.
+  const body = await c.req.json();
+  const query = await c.req.queries("param");
+  const headers = await c.req.header("Authorization");
+  console.log("body: ", body, "query: ", query, "headers: ", headers);
+  
+  return c.json({
+    message: "Hello World"
+  })
 })
 
 export default app
